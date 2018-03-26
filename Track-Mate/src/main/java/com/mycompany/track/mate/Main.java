e9152243b8446dd39b0ce2182eb438e2d9907248
fvandaalen <florianvandaalen@home.nl>
2018-03-26 19:57:34 +0200

    Project setup

---
+++ Track-Mate/src/main/java/com/mycompany/track/mate/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.track.mate;
+
+/**
+ *
+ * @author Florian
+ */
+public class Main {
+    public void main(String args[]){
+        System.out.println("Totally tracking that idiot actor");
+    }
+}
